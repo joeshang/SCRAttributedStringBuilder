5999f529d381790082bcc9d517d55127902dc0e0
Joe Shang <shangchuanren@gmail.com>
2018-04-27 22:08:11 +0800

    MOD: 更新头文件

--- SCRAttributedStringBuilder/NSMutableAttributedString+SCRAttributedStringBuilder.h
+++ SCRAttributedStringBuilder/NSMutableAttributedString+SCRAttributedStringBuilder.h
@@ -13,124 +13,124 @@
 #pragma mark - Content
 
 // 创建一个 Attributed String
-+ (NSMutableAttributedString *(^)(NSString *))build;
++ (NSMutableAttributedString *(^)(NSString *string))build;
 
 // 尾部追加一个新的 Attributed String
-- (NSMutableAttributedString *(^)(NSString *))append;
+- (NSMutableAttributedString *(^)(NSString *string))append;
 
 // 在尾部添加图片附件，默认使用图片尺寸，图片垂直居中，为了设置处理垂直居中（基于字体的 capHeight），需要在添加图片附件之前设置字体
-- (NSMutableAttributedString *(^)(UIImage *))appendImage;
+- (NSMutableAttributedString *(^)(UIImage *image))appendImage;
 
 // 在尾部添加图片附件，可以自定义尺寸，其他同 appendImage
-- (NSMutableAttributedString *(^)(UIImage *, CGSize))appendSizeImage;
+- (NSMutableAttributedString *(^)(UIImage *image, CGSize imageSize))appendSizeImage;
 
 // 在头部添加图片附件，由于不确定字体信息，因此需要显式输入字体
-- (NSMutableAttributedString *(^)(UIImage *, CGSize, UIFont *))insertImage;
+- (NSMutableAttributedString *(^)(UIImage *image, CGSize imageSize, UIFont *font))insertImage;
 
 #pragma mark - Range
 
 // 根据 start 和 length 设置范围
-- (NSMutableAttributedString *(^)(NSInteger, NSInteger))range;
+- (NSMutableAttributedString *(^)(NSInteger location, NSInteger length))range;
 
 // 将范围设置为当前字符串全部
 - (NSMutableAttributedString *)all;
 
 // 匹配所有符合的字符串
-- (NSMutableAttributedString *(^)(NSString *))match;
+- (NSMutableAttributedString *(^)(NSString *string))match;
 
 // 从头开始匹配第一个符合的字符串
-- (NSMutableAttributedString *(^)(NSString *))matchFirst;
+- (NSMutableAttributedString *(^)(NSString *string))matchFirst;
 
 // 为尾开始匹配第一个符合的字符串
-- (NSMutableAttributedString *(^)(NSString *))matchLast;
+- (NSMutableAttributedString *(^)(NSString *string))matchLast;
 
 #pragma mark - Basic
 
 // 字体
-- (NSMutableAttributedString *(^)(UIFont *))font;
+- (NSMutableAttributedString *(^)(UIFont *font))font;
 
 // 字号，默认字体
-- (NSMutableAttributedString *(^)(CGFloat))fontSize;
+- (NSMutableAttributedString *(^)(CGFloat fontSize))fontSize;
 
 // 字体颜色
-- (NSMutableAttributedString *(^)(UIColor *))color;
+- (NSMutableAttributedString *(^)(UIColor *color))color;
 
 // 字体颜色，16 进制
-- (NSMutableAttributedString *(^)(NSInteger))hexColor;
+- (NSMutableAttributedString *(^)(NSInteger hex))hexColor;
 
 // 背景颜色
-- (NSMutableAttributedString *(^)(UIColor *))backgroundColor;
+- (NSMutableAttributedString *(^)(UIColor *color))backgroundColor;
 
 #pragma mark - Glyph
 
 // 删除线风格
-- (NSMutableAttributedString *(^)(NSUnderlineStyle))strikethroughStyle;
+- (NSMutableAttributedString *(^)(NSUnderlineStyle style))strikethroughStyle;
 
 // 删除线颜色
-- (NSMutableAttributedString *(^)(UIColor *))strikethroughColor;
+- (NSMutableAttributedString *(^)(UIColor *color))strikethroughColor;
 
 // 下划线风格
-- (NSMutableAttributedString *(^)(NSUnderlineStyle))underlineStyle;
+- (NSMutableAttributedString *(^)(NSUnderlineStyle style))underlineStyle;
 
 // 下划线颜色
-- (NSMutableAttributedString *(^)(UIColor *))underlineColor;
+- (NSMutableAttributedString *(^)(UIColor *color))underlineColor;
 
 // 字形边框颜色
-- (NSMutableAttributedString *(^)(UIColor *))strokeColor;
+- (NSMutableAttributedString *(^)(UIColor *color))strokeColor;
 
 // 字形边框宽度
-- (NSMutableAttributedString *(^)(CGFloat))strokeWidth;
+- (NSMutableAttributedString *(^)(CGFloat width))strokeWidth;
 
 // 字体效果
-- (NSMutableAttributedString *(^)(NSString *))textEffect;
+- (NSMutableAttributedString *(^)(NSString *effect))textEffect;
 
 // 阴影
-- (NSMutableAttributedString *(^)(NSShadow *))shadow;
+- (NSMutableAttributedString *(^)(NSShadow *shadow))shadow;
 
 // 链接
-- (NSMutableAttributedString *(^)(NSURL *))link;
+- (NSMutableAttributedString *(^)(NSURL *url))link;
 
 #pragma mark - Paragraph
 
 // 行间距
-- (NSMutableAttributedString *(^)(CGFloat))lineSpacing;
+- (NSMutableAttributedString *(^)(CGFloat spacing))lineSpacing;
 
 // 段间距
-- (NSMutableAttributedString *(^)(CGFloat))paragraphSpacing;
+- (NSMutableAttributedString *(^)(CGFloat spacing))paragraphSpacing;
 
 // 对齐
-- (NSMutableAttributedString *(^)(NSTextAlignment))alignment;
+- (NSMutableAttributedString *(^)(NSTextAlignment alignment))alignment;
 
 // 换行
-- (NSMutableAttributedString *(^)(NSLineBreakMode))lineBreakMode;
+- (NSMutableAttributedString *(^)(NSLineBreakMode mode))lineBreakMode;
 
 // 段第一行头部缩进
-- (NSMutableAttributedString *(^)(CGFloat))firstLineHeadIndent;
+- (NSMutableAttributedString *(^)(CGFloat indent))firstLineHeadIndent;
 
 // 段头部缩进
-- (NSMutableAttributedString *(^)(CGFloat))headIndent;
+- (NSMutableAttributedString *(^)(CGFloat indent))headIndent;
 
 // 段尾部缩进
-- (NSMutableAttributedString *(^)(CGFloat))tailIndent;
+- (NSMutableAttributedString *(^)(CGFloat indent))tailIndent;
 
 // 行高，为了实现 Sketch 效果，会根据当前字体对 baselineOffset 进行修正
-- (NSMutableAttributedString *(^)(CGFloat))lineHeight;
+- (NSMutableAttributedString *(^)(CGFloat lineHeight))lineHeight;
 
 #pragma mark - Special
 
 // 基线偏移
-- (NSMutableAttributedString *(^)(CGFloat))baselineOffset;
+- (NSMutableAttributedString *(^)(CGFloat offset))baselineOffset;
 
 // 连字
-- (NSMutableAttributedString *(^)(CGFloat))ligature;
+- (NSMutableAttributedString *(^)(CGFloat ligature))ligature;
 
 // 字间距
-- (NSMutableAttributedString *(^)(CGFloat))kern;
+- (NSMutableAttributedString *(^)(CGFloat kern))kern;
 
 // 倾斜
-- (NSMutableAttributedString *(^)(CGFloat))obliqueness;
+- (NSMutableAttributedString *(^)(CGFloat obliqueness))obliqueness;
 
 // 扩张
-- (NSMutableAttributedString *(^)(CGFloat))expansion;
+- (NSMutableAttributedString *(^)(CGFloat expansion))expansion;
 
 @end
--- SCRAttributedStringBuilderDemo/SCRAttributedStringBuilderDemo/ViewController.m
+++ SCRAttributedStringBuilderDemo/SCRAttributedStringBuilderDemo/ViewController.m
@@ -24,11 +24,12 @@
     shadow.shadowOffset = CGSizeMake(2, 2);
     NSString *text = @"测试多行文字测试多行文字测试多行文字链接测试多行文字测试多行文字链接测试多行文字测试多行文字测试多行文字链接测试多行文字测试多行文字测试多行文字\n";
     self.label.attributedText = @"颜色/字体\n".attributedBuild.fontSize(30).color([UIColor purpleColor])
+        .range(1, 1).color([UIColor redColor])
         .append(text).firstLineHeadIndent(20).lineHeight(25).paragraphSpacing(20)
         .match(@"链接").hexColor(0xFF4400).backgroundColor([UIColor lightGrayColor])
         .matchFirst(@"链接").underlineStyle(NSUnderlineStyleThick).underlineColor([UIColor greenColor])
         .matchLast(@"链接").strikethroughStyle(NSUnderlineStyleSingle).strikethroughColor([UIColor yellowColor])
-        .append(text).alignment(NSTextAlignmentCenter).lineSpacing(10)
+        .append(text).alignment(NSTextAlignmentCenter).headIndent(20).tailIndent(-20).lineSpacing(10)
         .append(@"路飞").font([UIFont systemFontOfSize:25]).strokeWidth(2).strokeColor([UIColor darkGrayColor])
         .appendSizeImage([UIImage imageNamed:@"luffer"], CGSizeMake(50, 50))
         .insertImage([UIImage imageNamed:@"luffer"], CGSizeMake(50, 50), [UIFont systemFontOfSize:25])
